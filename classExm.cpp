299089726576f18cce841c3f686104146fa0882e
Red74Moon <fedoormaster@gmail.com>
2021-10-08 22:57:49 +0900

    classExm.cpp 파일 생성

---
+++ classExm.cpp
@@ -0,0 +1,16 @@
+//클래스 내에서만 접근이 가능한 HP, MP, Skill를 가진 Monster 클래스를 만든다.
+// HP, MP, Skill 멤버 변수는 이름이 Info인 멤버 함수를 통해 값을 입력받아 출력된다.
+//멤버 함수는 클래스 외부에서도 접근이 가능하다.
+//
+//Monster1을 만들어 멤버 함수 Info 를 통해  
+//HP는 100 MP는 50, Skill 이름은 ThunderBolt를 대입하여 출력하시오.
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
