aa473a08b39dadb2bc7a7d901179abca3e613a7a
Red74Moon <fedoormaster@gmail.com>
2021-10-10 10:30:00 +0900

    class 연습 20211010

--- classExm.cpp
+++ classExm.cpp
@@ -1,50 +1,35 @@
-//클래스 내에서만 접근이 가능한 HP, MP, Skill를 가진 Monster 클래스를 만든다.
-// HP, MP, Skill 멤버 변수는 이름이 Info인 멤버 함수를 통해 값을 입력받아 출력된다.
-//멤버 함수는 클래스 외부에서도 접근이 가능하다.
-//
-//Monster1을 만들어 멤버 함수 Info 를 통해  
-//HP는 100 MP는 50, Skill 이름은 ThunderBolt를 대입하여 출력하시오.
-
-
 #include <iostream>
-#include <cstring>
 
 using namespace std;
 
-class Monster
+class Charcter
 {
 public:
-	Monster();
-	~Monster();
-	void Info(int, int, string);
-
-private:
-	int HP;
-	int MP;
-	string Skill;
+	int hp;
+	int speed;
+	string name;
+	void move(int dir);
+	void jump();
+	void hit();
 };
 
 int main()
 {
-	Monster Monster1;
+	Charcter mario;
 
-	Monster1.Info(100, 50, "ThunderBolt");
-
+	mario.name = "슈퍼마리오";
+	mario.jump();
 	return 0;
 }
 
-Monster::Monster()
+void Charcter::move(int dir)
 {
 }
 
-Monster::~Monster()
+void Charcter::jump()
 {
 }
 
-void Monster::Info(int i, int j, string c)
+void Charcter::hit()
 {
-	HP = i;
-	MP = j;
-	Skill = c;
-	cout << i << '\t' << j << '\t' << c << endl;
 }
