6ead1e2ac492ff0f76805baf1bcf3ebf60ec40d8
Red74Moon <fedoormaster@gmail.com>
2021-10-10 10:41:02 +0900

    getter setter

--- classExm.cpp
+++ classExm.cpp
@@ -4,49 +4,28 @@ using namespace std;
 
 class Charcter
 {
-public:
+private:
 	int hp;
-	int speed;
-	string name;
-	void move(int dir);
-	void jump();
-	void hit();
 
-	void speedUp();
-	void obtainItem();
+public:
+	void setHp(int _hp);
+	int getHp();
 };
 
 int main()
 {
 	Charcter mario;
-
-	mario.name = "½´ÆÛ¸¶¸®¿À";
-	mario.jump();
-
-	mario.obtainItem();
-	mario.speed += 10000;
-
+	mario.setHp(100);
+	cout << mario.getHp() << endl;
 	return 0;
 }
 
-void Charcter::move(int dir)
+void Charcter::setHp(int _hp)
 {
+	hp = _hp;
 }
 
-void Charcter::jump()
+int Charcter::getHp()
 {
-}
-
-void Charcter::hit()
-{
-}
-
-void Charcter::speedUp()
-{
-	speed++;
-}
-
-void Charcter::obtainItem()
-{
-	speedUp();
+	return 0;
 }
