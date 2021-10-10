f940e45c1d77ccc53568c4f7e8ee1b8108dadb04
Red74Moon <fedoormaster@gmail.com>
2021-10-10 11:00:40 +0900

    복사생성자

--- classExm.cpp
+++ classExm.cpp
@@ -2,48 +2,36 @@
 
 using namespace std;
 
-class Charcter
+class Character
 {
 public:
-	Charcter();
-	~Charcter();
-	Charcter(int hp);
-	void setHp(int _hp);
+	Character(int _hp)
+	{
+		hp = _hp;
+	}
+	Character(Character& _Character)
+	{
+		hp = _Character.hp;
+	}
+
 	int getHp();
+
 private:
 	int hp;
-
 };
 
 int main()
 {
-	Charcter mario;
-	mario.setHp(100);
+	Character mario(100);
 	cout << mario.getHp() << endl;
+
+	Character luigi(mario);
+	cout << luigi.getHp() << endl;
+
 	return 0;
 }
 
-Charcter::Charcter()
+int Character::getHp()
 {
-	hp = 100;
-}
-
-Charcter::~Charcter()
-{
-}
-
-
-Charcter::Charcter(int _hp)
-{
-	hp = _hp;
-}
-
-void Charcter::setHp(int _hp)
-{
-	hp = _hp;
-}
-
-int Charcter::getHp()
-{
-	return 0;
+	return hp;
 }
