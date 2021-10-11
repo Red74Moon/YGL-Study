c93d57f61f4d687ead75b3f082deef8d760d2bd1
Red74Moon <fedoormaster@gmail.com>
2021-10-11 10:26:29 +0900

    string symbolic constants v1

--- classExm.cpp
+++ classExm.cpp
@@ -2,33 +2,10 @@
 
 using namespace std;
 
-class Weapon
-{
-public:
-	void Attack()
-	{
-		cout << "무기로 공격" << endl;
-	};
-protected:
-	int power;
-};
-
-class Knife : public Weapon
-{
-public:
-	void Attack()
-	{
-		cout << "칼로 공격" << endl;
-	};
-};
-
 int main()
 {
-	Weapon* weapon = new Knife();
-
-	((Knife*)weapon)->Attack();
-
-	delete weapon;
+	char myName[] = "Alex";
+	cout << myName;
 
 	return 0;
 }
