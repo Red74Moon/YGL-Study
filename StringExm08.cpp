8b931f2897c9cad2fde04226760219b6140c4c3e
Red74Moon <fedoormaster@gmail.com>
2021-10-15 22:51:32 +0900

    string-iofiles

--- StringExm08.cpp
+++ StringExm08.cpp
@@ -1,21 +1,48 @@
-// strtok
+// 입력 파일의 문자열을 읽어 들이고
+// 오른쪽으로 정렬한 출력 파일을 만드는 프로그램
 
 #include <iostream>
 #include <cstring>
+#include <fstream>
+#include <cassert>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	char str[] = "2021년 10월 15일";
-	char* p;
+	ifstream inputFile;
+	ofstream outputFile;
+	string line;
 
-	p = strtok(str, " ");
-	while (p)
+	inputFile.open("inFile.dat");
+	assert(inputFile);
+	int maxSize = 0;
+
+	while (!inputFile.eof())
 	{
-		cout << p << endl;
-		p = strtok(0, " ");
+		getline(inputFile, line);
+		if(line.size() > maxSize)
+		{ 
+			maxSize = line.size();
+		}
 	}
 
+	inputFile.close();
+
+	inputFile.open("inFile.dat");
+	assert(inputFile);
+	outputFile.open("outFile.dat");
+	assert(outputFile);
+	while (!inputFile.eof())
+	{
+		getline(inputFile, line);
+		string temp(maxSize - line.size(), ' ');
+		line.insert(0, temp);
+		line.append("\n");
+		outputFile << line;
+	}
+	inputFile.close();
+	outputFile.close();
 	return 0;
 }
