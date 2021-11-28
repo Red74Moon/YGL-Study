305e575dffe5bd4e4ac3ba18396d38880855b44b
Red74Moon <fedoormaster@gmail.com>
2021-11-28 19:53:56 +0900

    code 수정

--- 20211128/main.cpp
+++ 20211128/main.cpp
@@ -4,10 +4,18 @@ using namespace std;
 
 int main()
 {
-	const char* a = "abc";
+	//const char* a = "abc";
+
+	//cout << a << endl;
+	//cout << &a << endl;
+
+	char a[] = "abc";
+
+	char* b = &a[0];
 
 	cout << a << endl;
-	cout << &a << endl;
+	cout << b << endl;
+	cout << &b << endl;
 
 	return 0;
 }
